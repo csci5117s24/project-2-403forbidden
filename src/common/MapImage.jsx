910627df15726a441ff3147c693b5939298093df
RuichenHe <kylehe970128@gmail.com>
2024-04-17 22:57:07 -0500

    Update

---
+++ src/common/MapImage.jsx
@@ -0,0 +1,14 @@
+function MapImage({lat, lng}) {
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;  // Access the API key from environment variables
+    const size = "600x300";
+    const zoom = 13;
+    const maptype = "roadmap";
+    const centerCoords = `${lat},${lng}`;
+    const marker = `color:blue|${centerCoords}`;
+  
+    const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${centerCoords}&zoom=${zoom}&size=${size}&maptype=${maptype}&markers=${marker}&key=${apiKey}`;
+  
+    return (
+        <img src={mapUrl} alt="Dynamic Map" />
+    );
+  }
--- src/routes/Rangevisit_detail_page.jsx
+++ src/routes/Rangevisit_detail_page.jsx
@@ -18,6 +18,13 @@ function App() {
     const {rangevisit} = useLoaderData();
     console.log(rangevisit);
 
+    const [visitDate, setVisitDate] = useState(rangevisit.visitDate);
+    const [visitDuration, setVisitDuration] = useState(rangevisit.duration);
+    const [visitLat, setVisitLat] = useState(rangevisit.rangeLat);
+    const [visitLng, setVisitLng] = useState(rangevisit.rangeLng);
+    const [visitDetail, setVisitDetail] = useState(rangevisit.visitDetail)
+
+    
     return (
       <div className="main-container">
       </div>
